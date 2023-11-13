ba7622c0827a94e2099bf4364215adc918b93fa3
mabeya1 <bmabeya90@gmailcom>
2023-11-13 05:05:58 -0800

    This is the second correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 
 /**
- * binary_to_uint - Convert a binary string to an unsigned integer.
+ * binary_to_uint - Convert a binary string to an unsigned
+ * integer.
  * @b: The input binary string.
  *
- * This function converts a binary string 'b' to an unsigned integer.
+ * This function converts a binary string 'b' to an
+ * unsigned integer.
  * It iterates through each character in the string
  * If any character is not '0' or '1', the function returns 0.
  * The binary representation in 'b' is then converted to
